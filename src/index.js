ec35de0f52a8b489750775f29b637764e9f3c8a0
nlcdfpt <danhnlcse140655@fpt.edu.vn>
2021-07-01 14:11:29 +0700

    set up package node-sass and using it in project

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
 const path = require('path');
-const express = require('express')
+const express = require('express');
 const exphbs  = require('express-handlebars');
-const morgan = require('morgan')
-const app = express()
-const port = 3000
+const morgan = require('morgan');
+const app = express();
+const port = 3000;
+
+// Path static
+app.use(express.static(path.join(__dirname, 'public')));
 
 // HTTP logger
-app.use(morgan('combined'))
+app.use(morgan('combined'));
 
 // Template engine
 app.engine('hbs', exphbs({ extname: '.hbs' }));
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
 
-app.get('/', (req, res) => { res.render('home') })
-app.listen(port, () => console.log(`Example express listening http://localhost:${port}`))
+app.get('/', (req, res) => { res.render('home') });
+
+app.listen(port, () => console.log(`Example express listening http://localhost:${port}`));
