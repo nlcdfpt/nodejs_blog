354fd7ab77c5f501033c3d020483bd4ad50fca22
nlcdfpt <danhnlcse140655@fpt.edu.vn>
2021-07-01 08:45:04 +0700

    Set up and using package express-handlebars

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
+const path = require('path');
 const express = require('express')
+const exphbs  = require('express-handlebars');
 const morgan = require('morgan')
 const app = express()
 const port = 3000
 
+// HTTP logger
 app.use(morgan('combined'))
 
-app.get('/', (req, res) => res.send(`<h1>Nguyen Lam Cong Danh</h1>`))
+// Template engine
+app.engine('hbs', exphbs({ extname: '.hbs' }));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resources/views'));
+
+app.get('/', (req, res) => { res.render('home') })
 app.listen(port, () => console.log(`Example express listening http://localhost:${port}`))
