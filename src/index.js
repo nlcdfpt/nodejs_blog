36f8254067a968dc1cca5275a667a12180dc931b
nlcdfpt <danhnlcse140655@fpt.edu.vn>
2021-07-05 11:37:09 +0700

    Add file new.hbs, search.hbs to practice with method GET and POST

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 const path = require('path');
 const express = require('express');
-const exphbs  = require('express-handlebars');
+const exphbs = require('express-handlebars');
 const morgan = require('morgan');
 const app = express();
 const port = 3000;
+
+// Middleware to convert HTTP post with body
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 // Path static
 app.use(express.static(path.join(__dirname, 'public')));
@@ -17,5 +21,14 @@ app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
 
 app.get('/', (req, res) => { res.render('home') });
+app.get('/search', (req, res) => {
+    console.log('Result: ', req.query);
+    res.render('search');
+});
+app.post('/search', (req, res) => {
+    console.log('Result with post: ', req.body);
+    res.render('search');
+});
+app.get('/news', (req, res) => { res.render('news') });
 
 app.listen(port, () => console.log(`Example express listening http://localhost:${port}`));
